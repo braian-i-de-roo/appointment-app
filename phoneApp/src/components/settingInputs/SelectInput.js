be7b787ece614a4d4c4116b01ce1365c22f8a4c7
Braian De Roo <braian.de.roo@squad.com.ar>
2021-08-28 13:41:00 -0300

    Updated settings styles

--- phoneApp/src/components/settingInputs/SelectInput.js
+++ phoneApp/src/components/settingInputs/SelectInput.js
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
 
-import {View} from 'react-native';
-import {Select, SelectItem, Text} from '@ui-kitten/components';
+import {Dimensions, Pressable, StyleSheet, View} from 'react-native';
+import {Card, Icon, Menu, MenuItem, Modal, Text} from '@ui-kitten/components';
+
+const screenWidth = Dimensions.get('screen').width;
 
 const SelectInput = props => {
   const [selectedIndex, setSelectedIndex] = useState();
-  console.log('index', selectedIndex);
-  console.log('value', props.value);
-  console.log('default', props.default);
+  const [modalVisible, setModalVisible] = useState(false);
   const displayValue = selectedIndex
     ? props.possibleValues[selectedIndex.row]
     : props.value
@@ -17,23 +17,80 @@ const SelectInput = props => {
     : undefined;
 
   const onSelect = index => {
+    setModalVisible(false);
     setSelectedIndex(index);
     props.setter(props.possibleValues[index.row]);
   };
   return (
-    <View>
-      <Text config="h6">{props.name}</Text>
-      <Select
-        placeholder={props.name}
-        selectedIndex={selectedIndex}
-        value={displayValue}
-        onSelect={onSelect}>
-        {props.possibleValues.map(x => {
-          return <SelectItem key={x} title={x} />;
-        })}
-      </Select>
-    </View>
+    <Pressable
+      style={({pressed}) => [
+        {
+          backgroundColor: pressed ? 'rgba(0, 0, 0, 0.1)' : 'rgba(0, 0, 0, 0)',
+          height: 60,
+          marginHorizontal: 5,
+          paddingHorizontal: 5,
+          borderRadius: 5,
+        },
+      ]}
+      onPress={() => {
+        setModalVisible(true);
+      }}>
+      <View style={styles.horizontalContainer}>
+        <View style={styles.textContainer}>
+          <Text category="h6">{props.name}</Text>
+          <Text appearance="hint">{props.description}</Text>
+        </View>
+        <View style={styles.selectWrapper}>
+          <Text appearance="hint">{displayValue}</Text>
+          <Icon name="code" style={styles.icon} fill="#8F9BB3" />
+        </View>
+      </View>
+      <Modal
+        visible={modalVisible}
+        backdropStyle={styles.modalBackdrop}
+        onBackdropPress={() => {
+          setModalVisible(false);
+        }}>
+        <Card disabled={true} style={{width: (80.0 * screenWidth) / 100}}>
+          <Menu selectedIndex={selectedIndex} onSelect={onSelect}>
+            {props.possibleValues.map(x => {
+              return <MenuItem key={x} title={x} />;
+            })}
+          </Menu>
+        </Card>
+      </Modal>
+    </Pressable>
   );
 };
+
+const styles = StyleSheet.create({
+  pressable: {
+    height: 60,
+    marginHorizontal: 10,
+  },
+  icon: {
+    height: 16,
+    width: 16,
+    transform: [{rotate: '90deg'}],
+  },
+  horizontalContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  textContainer: {
+    width: '60%',
+    justifyContent: 'center',
+  },
+  selectWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  modalBackdrop: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+});
 
 export default SelectInput;
