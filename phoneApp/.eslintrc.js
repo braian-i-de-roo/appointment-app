6623d1230d96075ac7d77ef2cac622d2a06adfbf
Braian De Roo <braian.de.roo@squad.com.ar>
2021-09-11 09:30:19 -0300

    Add missing configurations

--- phoneApp/.eslintrc.js
+++ phoneApp/.eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'prettier'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['airbnb-typescript'],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
 };
